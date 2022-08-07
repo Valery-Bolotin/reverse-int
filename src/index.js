8b72263b850f487772a44b14c6ef55c38b7378a4
Valery-Bolotin <bolotinval86@gmail.com>
2022-08-07 23:51:45 +0300

    fix: finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n, r = 0) {
+    var x = Math.abs(n)
+    if(!x) return r;
+    r =  r * 10  + x % 10;
+    return reverse(Math.floor( x / 10), r);
+    
+    
 }
+
